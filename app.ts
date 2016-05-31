4915f299449cd80dee665375e58c0d12a5390af1
Rob Quist <rob.quist@dukesoft.nl>
2016-05-31 15:47:08 +0200

    Module scanning working

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 /// <reference path="./typings/index" />
 import * as PiStation from "./node_modules/pistation-definitions/PiStation.ts";
 import {Server, ServerEvent} from './app/server';
-import {Dummy} from "./modules/module-dummy/dummy.module";
 import * as Rx from 'rxjs/Rx';
+import fs = require('fs');
 
 const app = new Server();
 
@@ -15,5 +15,38 @@ const module = new PiStation.Module(
 );
 
 //app.addModule(module);
+var moduleDir = './modules';
+fs.readdir(moduleDir, function( err, files ) {
+    if (err) {
+        console.error("Could not list the module directory.", err);
+        process.exit(1);
+    }
+
+    files.forEach( function( file, index ) {
+        // Make one pass and make the file complete
+        var moduleName = moduleDir + '/' + file;
+        fs.stat(moduleName, function( error, stat ) {
+            if( error ) {
+                console.error( "Error stating file.", error );
+                return;
+            }
+
+            if( stat.isDirectory() ) {
+                fs.readFile(moduleName + '/module.json', 'utf8', function (err, data) {
+                    if (err) throw err; // we'll not consider error handling for now
+                    var obj = JSON.parse(data);
+                    if (obj.pistationVersion != '1') {
+                        console.log('"%s" is not a valid PiStation module');
+                        return;
+                    }
+                    console.log('Found module "%s" from "%s"', obj.name, obj.author);
+                    //Load it up
+                });
+            }
+        });
+    });
+});
+
+import {Dummy} from "./modules/module-dummy/dummy.module";
 const dummyModule : Dummy = new Dummy();
 app.addModule(dummyModule);
