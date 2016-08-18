f6fbb70ff50adfd2285ff9ecd7358dd4abb7e6c5
Robbert van Markus <robbert@vanmarkus.com>
2016-08-19 01:05:21 +0200

    - some refactoring server bootloader - added levelup database inc typings

--- app.ts
+++ app.ts
@@ -1,28 +1,39 @@
 /// <reference path="./typings/index" />
+/// <reference path="./typings/main" />
 import * as PiStation from "./node_modules/pistation-definitions/PiStation.ts";
 import {Server, ServerEvent} from './app/server';
 import * as Rx from 'rxjs/Rx';
 import * as Modules from './modules/autoload';
 import * as Connectors from './connectors/autoload';
 
-const app = new Server();
 
-class PiStationServer {
+class PiStationBootloader {
+    private serverInstance : Server;
     constructor() {
+        this.serverInstance = new Server();
+        this.initConnectors();
+        this.initModules();
+    }
+
+    initConnectors(){
+        for (var i in Connectors) {
+            if (typeof Connectors[i] == 'function') {
+                let connector = new Connectors[i](this.serverInstance);
+                this.serverInstance.addConnector(connector);
+            }
+        }
 
     }
+    initModules(){
+        for (var i in Modules) {
+            if (typeof Modules[i] == 'function') {
+                let module = new Modules[i](this.serverInstance);
+                this.serverInstance.addModule(module);
+            }
+        }
+    }
+
+
 }
 
-for (var i in Connectors) {
-    if (typeof Connectors[i] == 'function') {
-        let connector = new Connectors[i](app);
-        app.addConnector(connector);
-    }
-}
-
-for (var i in Modules) {
-    if (typeof Modules[i] == 'function') {
-        let module = new Modules[i](app);
-        app.addModule(module);
-    }
-}
+const piServer = new PiStationBootloader();
--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,7 @@
 import * as io from "socket.io";
 import * as PiStation from "../node_modules/pistation-definitions/PiStation.ts";
 import * as  Rx from 'rxjs/Rx';
+var levelup = require('levelup');
 import Socket = SocketIO.Socket;
 import {Module} from "./module";
 import {Connector} from "./connector";
@@ -14,38 +15,60 @@ export class Server {
     private socketServer:SocketIO.Server;
     private modules:PiStation.Module[] = [];
     private connectors:PiStation.Connector[] = [];
+    private db : LevelUp;
 
     public clientConnections:Rx.Observable<SocketIO.Socket>;
 
     constructor(private port:number = 31415) {
-        this.socketServer = io(port);
         console.log('Server Started');
+        this.db = levelup('./pistationData');
 
-        this.clientConnections = Rx.Observable.create((observer : any) => {
-            this.socketServer.on(`${PiStation.Events.CLIENT_CONNECTED}`, (socket : SocketIO.Socket) => observer.next(socket));
+        // 2) put a key & value
+        this.db.put('name', 'LevelUP', (err) => {
+            if (err) return console.log('Ooops!', err) // some kind of I/O error
 
-            this.socketServer.on('error', (error : any) => {
-                console.log('ERROR', error);
-                observer.error(error)
-            });
-        });
+            // 3) fetch by key
+            this.db.get('name', function (err, value) {
+                if (err) return console.log('Ooops!', err) // likely the key was not found
+
+                // ta da!
+                console.log('name=' + value)
+            })
+        })
+
+        this.socketServer = io(port);
+        this.initClientSocketConnections();
+        this.subscribeForGlobalClientEvents();
 
         this.clientConnections
             .forEach((socket : SocketIO.Socket) => {
                 console.log(`New client connection | ID: ${socket.client.id} IP address: ${socket.client.conn.remoteAddress}`)
-                this.registerEventsForClient(socket)
+                this.registerModuleEventsForClient(socket)
             });
+    }
 
-        this.on(`${PiStation.Events.GET_ALL_MODULES}`).subscribe( (event : ServerEvent) => {
+    private subscribeForGlobalClientEvents() {
+        this.on(`${PiStation.Events.GET_ALL_MODULES}`).subscribe((event:ServerEvent) => {
             let data = this.modules.map(module => module.toDto());
-            console.log('Returning modules:',  data);
+            console.log('Returning modules:', data);
             event.socket.emit(`${PiStation.Events.GET_ALL_MODULES}`, data);
         });
 
-        this.on(`${PiStation.Events.GET_ALL_ACTIONS}`).subscribe((event : ServerEvent) => {
+        this.on(`${PiStation.Events.GET_ALL_ACTIONS}`).subscribe((event:ServerEvent) => {
             console.log('Asking all actions');
         });
-    }
+    };
+
+    private initClientSocketConnections() {
+        this.clientConnections = Rx.Observable.create((observer:any) => {
+            this.socketServer.on(`${PiStation.Events.CLIENT_CONNECTED}`, (socket:SocketIO.Socket) => observer.next(socket));
+
+            this.socketServer.on('error', (error:any) => {
+                console.log('ERROR', error);
+                observer.error(error)
+            });
+        });
+    };
 
     addModule(module:Module) {
        return this.modules.push(module);
@@ -57,7 +80,7 @@ export class Server {
 
     getConnector(connectorName:string) {
 
-        return this.connectors.find(connectorObject => (connectorObject.name === connectorName));
+        return this.connectors.filter(connectorObject => (connectorObject.name === connectorName));
     }
 
     getModule(moduleName:string) {
@@ -71,7 +94,7 @@ export class Server {
                     .map((data: any) => <ServerEvent>{data: data, socket: socket}));
     }
 
-    private registerEventsForClient(socket:SocketIO.Socket){
+    private registerModuleEventsForClient(socket:SocketIO.Socket){
         this.modules
             .forEach((module : Module) => { module.registerFunctionCallsForClient(socket)});
     }
