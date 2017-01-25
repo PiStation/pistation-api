4d526bd3d963af1066dc28278e52f622804ea779
Robbert van Markus <robbert@vanmarkus.com>
2017-01-25 04:07:42 +0100

    test mqtt events

--- moscaServer.ts
+++ moscaServer.ts
@@ -43,8 +43,36 @@ export class MoscaServer {
     }
     static Initialize(){
         return new MoscaServer()
-            .start().then((event) => {
+            .start().then((server) => {
                 $log.info('Mosca Server started...');
-            });
+                server.on('clientConnected', function(client) {
+                    console.log('client connected', client.id);
+                });
+
+// fired when a message is received
+                server.on('published', function(packet, client) {
+                    console.log('Published : ', packet.payload);
+                });
+
+// fired when a client subscribes to a topic
+                server.on('subscribed', function(topic, client) {
+                    console.log('subscribed : ', topic);
+                });
+
+// fired when a client subscribes to a topic
+                server.on('unsubscribed', function(topic, client) {
+                    console.log('unsubscribed : ', topic);
+                });
+
+// fired when a client is disconnecting
+                server.on('clientDisconnecting', function(client) {
+                    console.log('clientDisconnecting : ', client.id);
+                });
+
+// fired when a client is disconnected
+                server.on('clientDisconnected', function(client) {
+                    console.log('clientDisconnected : ', client.id);
+                });
+            })
     }
 }
